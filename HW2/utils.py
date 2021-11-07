f93341e19513c0ee93e30d26a0f4868ba4c66c63
ReemS3 <86101529+ReemS3@users.noreply.github.com>
2021-11-07 21:32:55 +0100

    Update utils.py

--- HW2/utils.py
+++ HW2/utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-# warnings.filterwarnings('ignore')
+""" Class for functions that are used in multiple classes. """
 def sigmoid(x):
   return 1.0/(1+np.exp(-x))
 
 def sigmoidprime(x): 
+  """Assuming x has been computed with sigmoid first"""
   return x * (1 - x)
