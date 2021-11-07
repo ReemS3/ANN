5c8dce4648a39c7a028711d296466a79cbe736cd
ReemS3 <86101529+ReemS3@users.noreply.github.com>
2021-11-07 21:33:41 +0100

    Update mlp.py

--- HW2/mlp.py
+++ HW2/mlp.py
@@ -2,30 +2,32 @@ from perceptron import Perceptron
 from utils import sigmoidprime
 import numpy as np
 
+
 class MLP():
     def __init__(self):
-        self.hidden_layer = [Perceptron(2), Perceptron(2), Perceptron(2), Perceptron(2)]
+        self.hidden_layer = [Perceptron(2), Perceptron(
+            2), Perceptron(2), Perceptron(2)]
         self.output_layer = Perceptron(4)
         self.previous_activations = []
         self.output = 0
-        
+
     def backpropagate(self, target):
         error = self.output - target
+        # save the deltas in the right order
         deltas = []
-        error_output = sigmoidprime(self.output)*error      
+        error_output = sigmoidprime(self.output)*error
         for index in range(len(self.hidden_layer)):
-
-            deltas.append(self.previous_activations[index]* error)
+            deltas.append(self.previous_activations[index] * error)
         deltas.append(error_output)
+        # add the output at the last
         self.output_layer.update(deltas)
 
-    
     def forwardpropagate(self, input):
         self.previous_activations = []
         for perpectron in self.hidden_layer:
             output = perpectron.forward_step(input)
-            # self.deriviative_previous_activations.append(output)    
             self.previous_activations.append(output)
-        previous_activations_reshaped = np.reshape(self.previous_activations, newshape=(-1))
-        self.output = self.output_layer.forward_step(previous_activations_reshaped)
-        # self.previous_activations.append(output)
+        previous_activations_reshaped = np.reshape(
+            self.previous_activations, newshape=(-1))
+        self.output = self.output_layer.forward_step(
+            previous_activations_reshaped)
